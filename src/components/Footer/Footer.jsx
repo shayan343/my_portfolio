e3a8fe4d6906f8df5a87aebc7ba0f9f5e4d07ae6
Shayan Alam <shayanalam343@gmail.com>
2021-09-26 22:55:42 +0530

    my portfolio

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
@@ -38,9 +38,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Developed by {' '}
+          <a href="https://github.com/shayan343/" target="_blank" rel="noopener noreferrer">
+            Shayan Alam
           </a>
         </p>
 
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,15 +2,15 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'shayanAlam', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
+  name: 'Shayan Alam',
   subtitle: '',
   cta: '',
 };
@@ -18,40 +18,46 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: `I am a Final Year Software Engineering UnderGrad based in Greater Noida.
+  I have great intrest in DSA and System Design along with computer science fundamentals.
+  `,
+  paragraphTwo: `I am skilled in various Frontend, Backend as well as Cloud Technologies.
+  My tech stack includes but is not limited to - HTML, CSS, JavaScript, React, Node.js, Express,
+  PostgreSQL, MongoDB and Azure.
+  `,
+  paragraphThree: 'Attatched below is my resume for further details...',
+  resume:
+    'https://docs.google.com/document/d/1hnoNXuM0zQUyQokC7liW07Itbc2gB2Joa_IFGwclQSY/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project1.png',
+    title: 'Sorting Algorithm Visualizer',
+    info: `◆ Built a web application in React to visualize sorting algorithms.
+    ◆ Implemented Merge Sort, Bubble Sort, Insertion Sort algorithm.
+    ◆ Managed the state of the app using Hooks API.
+    ◆ Made the app responsive using CSS Flexbox and React-Bootstrap.
+    `,
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://shayan343.github.io/sorting-visualizer/',
+    repo: 'https://github.com/shayan343/sorting-visualizer/tree/master/sorting-visualizer', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project2.jpg',
+    title: 'URL shortener',
+    info: `◆ A scalable backend service to shorten long URLs .
+    ◆ Developed using NodeJS, Express, Sequelize and PostgreSQL.
+    ◆ Capability to generate more than 1 Trillion unique short URLs.
+    ◆ Designed REST-style API endpoints for posting, fetching and
+    redirection of URLs.
+    `,
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://github.com/shayan343/url-shortener',
+    repo: 'https://github.com/shayan343/url-shortener', // if no repo, the button will not show up
   },
 ];
 
@@ -67,28 +73,24 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
+      name: 'instagram',
       url: '',
     },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'http://www.linkedin.com/in/shayan343',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/shayan343/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
